14dbc966dac86bb4e3a5852f18ba2ffaf0ed4b33
ashfak-rahad <ashfakur@gmail.com>
2023-06-09 16:07:06 +0600

    navbar added done

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/NavBar';
 
 const RootLayout = () => {
     return (
         <main>
+            <Navbar/>
             <Outlet/>
         </main>
     );
