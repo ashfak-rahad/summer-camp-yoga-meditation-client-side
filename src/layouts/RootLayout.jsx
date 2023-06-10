3fb012a11b7917730f6c2a7d983bc875004f2481
ashfak-rahad <ashfakur@gmail.com>
2023-06-10 18:37:19 +0600

    footer added done

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const RootLayout = () => {
     return (
         <main>
             <Navbar/>
             <div className='mt-[80px] mx-auto'><Outlet/></div>
+            <Footer/>
         </main>
     );
 };
