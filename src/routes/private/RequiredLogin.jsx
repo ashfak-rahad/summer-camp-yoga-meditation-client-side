1eaff5d86c77c63797049cafd400796762cbcddf
ashfak-rahad <ashfakur@gmail.com>
2023-06-12 01:16:40 +0600

    added RequiredLogin done

--- src/routes/private/RequiredLogin.jsx
+++ src/routes/private/RequiredLogin.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider';
+import LoadingSpinner from '../../spinner/LoadingSpinner';
 
-const RequiredLogin = () => {
-    return (
-        <div>
-            <h1>requiredLogin</h1>
-        </div>
-    );
+
+
+
+
+const RequiredLogin = ({children}) => {
+    const {loading, user} = useAuth()
+    const location = useLocation();
+    if(loading){
+        return <LoadingSpinner/>
+    }
+
+    if(user){
+        return children;
+    }
+    return <Navigate state={{from: location}} to="/login" replace/>;
 };
 
 export default RequiredLogin;
