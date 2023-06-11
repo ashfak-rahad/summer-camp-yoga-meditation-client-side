4a6dbc2609afaa5555e7d43618d7cd3b55d51517
ashfak-rahad <ashfakur@gmail.com>
2023-06-12 01:13:37 +0600

    adminOnly added done

--- src/routes/private/AdminOnly.jsx
+++ src/routes/private/AdminOnly.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider';
+import useAuthorization from '../../hooks/useAuthorization';
 
-const AdminOnly = () => {
-    return (
-        <div>
-            <h1>adminOnly</h1>
-        </div>
-    );
+
+
+
+
+
+
+
+
+const AdminOnly = ({children}) => {
+    const {loading, user} = useAuth()
+    const {isLoading, role} = useAuthorization()
+    const location = useLocation();
+    if(loading || isLoading){
+        return 
+    }
+    
+    if(user.email && role === "admin"){
+        return children;
+    }
+    return <Navigate state={{from: location}} to="/" replace/>;
 };
 
 export default AdminOnly;
