69a69e36c2ed215a261f28970d6d3654e49e7a8b
ashfak-rahad <ashfakur@gmail.com>
2023-06-14 00:45:53 +0600

    use classe hooks added done

---
+++ src/hooks/useMyClasses.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+import { useAuth } from '../context/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
+
+const useMyClasses = () => {
+    const {user, loading} = useAuth()
+    const {axiosSecure} = useAxiosSecure()
+    const {data: classes = [], refetch} = useQuery({
+        queryKey: ["my-classes", user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/my-classes?email=${user?.email}`)
+            return res.data
+        }
+    })
+    return {classes, refetch}
+};
+
+export default useMyClasses;
